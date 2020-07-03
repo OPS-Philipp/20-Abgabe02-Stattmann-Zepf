5dbb9c1b12fedd9e9737e6d26b39b19186a66a42
OPS-Philipp <philipp.stattmann@edu.campus02.at>
2020-07-03 14:26:15 +0200

    Method Setup() added. Methods for testing Offer added. First bug fixed: typo in maxSize in Constructor

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -3,10 +3,34 @@ package at.fhj.iit;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class StringQueueTest {
+import java.util.ArrayList;
+import java.util.List;
 
-   // TODO Write your own tests
+class StringQueueTest {
+    private StringQueue firstList;
+    private StringQueue secondList;
+
+    @BeforeEach
+    void setup() {
+        // SETUP PHASE
+        firstList = new StringQueue(1);
+        secondList = new StringQueue(6);
+    }
+
+    @Test
+    @DisplayName("Testing Offer")
+    public void testOffer() {
+        assertTrue(firstList.offer("Auto"), "Expected offer to return true");
+    }
+
+    @Test
+    @DisplayName("Testing Offer overflow")
+    public void testOfferOverflow() {
+        assertTrue(firstList.offer("Auto"), "Expected offer to return true");
+        assertFalse(firstList.offer("Bahn"), "Expected offer to return false");
+    }
 
 }
