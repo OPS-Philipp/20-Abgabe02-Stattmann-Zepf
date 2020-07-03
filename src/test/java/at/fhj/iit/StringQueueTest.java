c621644ac2e6c7e4709a3a789e1e13ca14f0043b
LukasZepf <lukas.zepf@edu.campus02.at>
2020-07-03 14:29:50 +0200

    Added Tests for method poll()

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -33,4 +33,26 @@ class StringQueueTest {
         assertFalse(firstList.offer("Bahn"), "Expected offer to return false");
     }
 
+    @Test
+    @DisplayName("Testing Poll")
+    public void testPoll() {
+        assertTrue(secondList.offer("Auto"), "Expected offer to return true");
+        assertTrue(secondList.offer("Bahn"), "Expected offer to return true");
+        assertTrue(secondList.offer("Bus"), "Expected offer to return true");
+        assertEquals(secondList.poll(), "Auto");
+        assertEquals(secondList.poll(), "Bahn");
+        assertEquals(secondList.poll(), "Bus");
+    }
+
+    @Test
+    @DisplayName("Testing Poll without element")
+    public void testEmptyPoll() {
+        assertNull(firstList.poll(), "Null expectet because List is empty");
+    }
+
+    @Test
+    @DisplayName("Testing Remove")
+    public void testRemove() {
+        //ToDo
+    }
 }
