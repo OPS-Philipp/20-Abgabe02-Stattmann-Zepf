5753adc00143a31f0cbc0cb6f874e1a41c8a7eac
LukasZepf <lukas.zepf@edu.campus02.at>
2020-07-04 18:39:56 +0200

    Added GenericQueue, to handle all kinds of datatypes

---
+++ src/main/java/at/fhj/iit/GenericQueue.java
@@ -0,0 +1,65 @@
+package at.fhj.iit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class GenericQueue<T> implements IQueue<T> {
+
+    private List<T> elements = new ArrayList<T>();
+    private int maxSize = 5;
+
+    public void StringQueue(int maxsize) {
+        maxSize = maxsize;
+    }
+
+    @Override
+    public boolean offer(T obj) {
+        if (elements.size() != maxSize)
+            elements.add(obj);
+        else
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public T poll() {
+        T element = peek();
+
+        if (elements.size() != 0) {
+            elements.remove(0);
+        }
+
+        return element;
+    }
+
+    @Override
+    public T remove() {
+        T element = poll();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
+
+        return element;
+    }
+
+    @Override
+    public T peek() {
+        T element;
+        if (elements.size() > 0)
+            element = elements.get(0);
+        else
+            element = null;
+
+        return element;
+    }
+
+    @Override
+    public T element() {
+        T element = peek();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
+
+        return element;
+    }
+}
